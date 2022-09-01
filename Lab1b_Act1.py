c1fa65b53cebba9905161556118a35838481cc95
CalebLewis0917 <95330126+CalebLewis0917@users.noreply.github.com>
2022-09-01 14:54:39 -0500

    Add files via upload

---
+++ Lab1b_Act1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Caleb Lewis
+# Section:      521
+# Assignment:   Lab 1b
+# Date:         30 Aug 2021
+#
+
+#
+# YOUR CODE HERE
+#
+print ("Howdy, World!")
+print ("I play in a metal band.")
